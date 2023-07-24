ed97003d89ae028578f212ecd5ef423f1ad4668f
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-25 01:15:05 +0530

    updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
-
+import { motion } from "framer-motion"
 
 type Props = {}
 
 export default function Header({ }: Props) {
     return (
-        <header>
-            <div className="flex flex-row {/*justify-evenly*/} items-center">
+        <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
+            <motion.div initial={{x: -1000, opacity: 0, scale: 0.5 }}
+                animate={{ x: 0, opacity: 1, scale: 1}} 
+                transition={
+                    {
+                        duration:1.5,
+
+                    }
+                }
+                className="flex flex-row {/*justify-evenly*/} items-center">
                 {/* Social Icons */}
                 {/* react social icons library 
             
@@ -19,12 +27,29 @@ export default function Header({ }: Props) {
                 <SocialIcon url="https://youtube.com/priyansu-nayak" fgColor="gray" bgColor="transparent" />
                 <SocialIcon url="https://twitter.com/priyansu-nayak" fgColor="gray" bgColor="transparent" />
                 <SocialIcon url="https://github.com/priyansu-nayak" fgColor="gray" bgColor="transparent" />
-                                
-            </div>
-            <div>
-                <SocialIcon className="cursor-pointer" network="email" fgColor="gray" bgColor="transparent"/>
-                <p className="uppercase">Get in Touch</p>
-            </div>
+
+            </motion.div>
+
+            <motion.div initial={
+                {
+                    x:500,opacity:0,scale:0.5
+                }
+            }
+            
+            animate={
+                {
+                    x:0, opacity:1,scale:1
+                }
+            }
+
+            transition={{
+                duration:1.5,
+            }}
+
+            className="flex flex-row items-center text-gray-300 cursor-pointer" >
+                <SocialIcon className="cursor-pointer" network="email" fgColor="gray" bgColor="transparent" />
+                <p className="uppercase hidden sm:inline-flex text-sm text-gray-400">Get in Touch</p>
+            </motion.div>
 
         </header>
     )
