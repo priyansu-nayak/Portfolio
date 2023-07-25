3b7a5fbef532b9d53c95e55444fe955f87d9cf9a
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-25 17:58:30 +0530

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
+import Link from 'next/link'
 // import BackgroundCircles from '@/components/BackgroundCircles'
 import Dp from "D:/Builds/portfolio-yt-2.0/Dp.jpg"
 type Props = {}
@@ -23,15 +24,30 @@ export default function Hero({ }: Props) {
             <img className="rounded-full relative w-36 h-36 mx-auto my-auto object-cover "
                 src="https://cdn.sanity.io/images/ltuexkre/production/a766c574c295f2103c63c2dfdf675557c664d016-600x600.png" alt="Profile Picture" />
 
-            <div>
+            <div className='z-20'>
                 <h2 className=" tracking-[8px] text-sm uppercase text-gray-500 pb-2">
                     Software Developer
                 </h2>
-                
+
                 <h1 className="text-5xl lg:text-6xl font-semibold px-10">
                     <span className='mr-3'>{text}</span>
                     <Cursor cursorColor='#F7AB0A'></Cursor>
                 </h1>
+
+                <div>
+                    <Link href="#About">
+                        <button className='heroButton'>About</button>
+                    </Link>
+                    <Link href="#Experience">
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+                    <Link href="#Skills">
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+                    <Link href="#Projects">
+                        <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
 
             </div>
 
