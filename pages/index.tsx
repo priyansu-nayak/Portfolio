ea6c6227f7f69206daf0291c55405d458b56fa59
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-25 13:49:08 +0530

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,19 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+    <div className="bg-[rgb(36,36,36)] text-white 
+    h-screen snap-y 
+    snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Priyansu's Portfolio</title>
       </Head>
 
       <Header />
-      {/* Hero */}
-      <section id="hero">
+      
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
+      
       {/* About */}
 
       {/* Experience */}
