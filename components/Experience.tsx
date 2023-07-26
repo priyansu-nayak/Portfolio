330ca097af5f019f3a1904233b92133ed4e40293
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-26 13:41:45 +0530

    updated

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -27,15 +27,15 @@ export default function Experience({ }: Props) {
 
             className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row 
         max-w-full px-10 justify-evenly mx-auto items-center' >
-            <h3 className='absolute top-24 uppercase 
+            <h3 className='  absolute top-24 uppercase 
     tracking-[20px] text-gray-500 text-2xl
     ' >Experience</h3>
 
-            <div>
+            <div className='mt-36 p-10 snap-mandatory snap-x  w-full flex space-x-5 overflow-x-scroll '>
                 <ExperienceCards/>
-                {/* <ExperienceCards/>
                 <ExperienceCards/>
-                <ExperienceCards/> */}
+                <ExperienceCards/>
+                <ExperienceCards/>
             </div>
 
         </motion.div>
--- components/ExperienceCards.tsx
+++ components/ExperienceCards.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function ExperienceCards({ }: Props) {
     return (
-        <article className=''>
+        <article className=' snap-center bg-[#292929] hover:cursor-pointer transiton-opacity duration-200  overflow-hidden hover:opacity-100  opacity-40 p-10      flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px]'>
             <motion.img
                 initial={{
                     y: -100,
@@ -22,7 +22,8 @@ export default function ExperienceCards({ }: Props) {
                 //viewport={{once:true}}
 
                 className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center "
-                src="https://cdn.sanity.io/images/ltuexkre/production/050ee674d199aa8d254af2b5ea480d3dc320cbb1-1240x1440.png" alt="Pic" />
+                src="https://cdn.sanity.io/images/ltuexkre/production/050ee674d199aa8d254af2b5ea480d3dc320cbb1-1240x1440.png" 
+                alt="Pic" />
 
             <div className="px-0 md:px-10">
                 <h4 className='text-4xl font-light mt-1 '>CEO of PAPAFAM</h4>
@@ -36,11 +37,11 @@ export default function ExperienceCards({ }: Props) {
                 <p className='uppercase py-5 text-gray-300'>Started work... -Ended...</p>
 
                 <ul className="list-disc space-y-4 ">
-                    <li> Summary points</li>
-                    <li> Summary points</li>
-                    <li> Summary points</li>
-                    <li> Summary points</li>
-                    <li> Summary points</li>
+                    <li> Summary points Summary pointsSummary pointsSummary pointsSummary points</li>
+                    <li> Summary points Summary pointsSummary pointsSummary pointsSummary points</li>
+                    <li> Summary points Summary pointsSummary pointsSummary pointsSummary points</li>
+                    <li> Summary points Summary pointsSummary pointsSummary pointsSummary points</li>
+                    <li> Summary points Summary pointsSummary pointsSummary pointsSummary points</li>
                 </ul>
             </div>
 
