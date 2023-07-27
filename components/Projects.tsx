893286fe714966d2d8052c953ca7d0ac3279f7f6
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-28 02:55:01 +0530

    updated

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
 type Props = {}
 
@@ -6,7 +7,12 @@ export default function Projects({ }: Props) {
 
   const projects = [1, 2, 3, 4, 5];
   return (
-    <div className='h-screen relative flex overflow-hidden 
+    <motion.div 
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:3}}
+
+    className='h-screen relative flex overflow-hidden 
     flex-col text-left md:flex-row  max-w-full  justify-evenly mx-auto items-center z-0 '>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500  text-2xl'>
         Projects
@@ -17,7 +23,23 @@ export default function Projects({ }: Props) {
         {projects.map((project, i) => (
           <div className='w-screen flex-shrink-0 flex flex-col 
           space-y-5 snap-center justify-center p-20 md:p-44 h-screen       '>
-            <img src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png"
+            <motion.img
+            initial={{
+              y:-300,
+            opacity:0
+            }}
+
+            transition={{
+              duration:1.2
+            }}
+
+            whileInView={{
+              opacity:1,
+              y:0
+            }}
+
+            
+            src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png"
               alt="Pic" />
 
             <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
@@ -27,7 +49,7 @@ export default function Projects({ }: Props) {
                 </span> UPS clone
               </h4>
 
-              <p>
+              <p className='text-lg text-center md:text-left'>
                 Netflix 2.e app that has a Log In and Log Out Authentication
                 with Google. It has a beautiful Home Screen with all the movies
                 looking just like Netflix. There is also a subscription page
@@ -43,9 +65,9 @@ export default function Projects({ }: Props) {
 
       </div>
 
-      <div className=" w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[300px] -skew-y-12 " />
+      <div className=" w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[400px] -skew-y-12 " />
 
-    </div>
+    </motion.div>
 
   );
 }
