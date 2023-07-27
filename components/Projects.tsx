626b2cb67eead2ea177040577d7ff35621eef1da
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-28 01:56:39 +0530

    updated

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,11 +3,38 @@ import React from 'react'
 type Props = {}
 
 export default function Projects({ }: Props) {
+
+  const projects = [1, 2, 3, 4, 5];
   return (
-    <div className='h-screen relative flex justify-center'>
-      <h1 className='absolute top-24 text-gray-500 mx-auto uppercase tracking-[20px] text-2xl'>
+    <div className='h-screen relative flex overflow-hidden 
+    flex-col text-left max-w-full  justify-evenly mx-auto items-center z-0 '>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500  text-2xl'>
         Projects
-      </h1>
+      </h3>
+
+      <div className='relative w-full flex overflow-x-scroll 
+      overflow-y-hidden snap-mandatory z-0 '>
+        {projects.map((project) => (
+          <div>
+            <img src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png"
+             alt="Pic" />
+          </div>
+        )
+        )}
+      
+
+
+
+
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 
+    left-0 h-[300px] -skew-y-12 ">
+
+      </div>
+
+
+
     </div>
   )
 }
