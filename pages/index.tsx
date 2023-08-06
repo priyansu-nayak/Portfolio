ce1bb197ee2e7db257ae4675328c2d3cb99c06ac
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-06 20:21:59 +0530

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ export default function Home() {
 
       <Link href='#hero'>
         <footer className='sticky bottom-5 w-full cursor-pointer'>
-          <div className="">
+          <div className=" flex items-center justify-center ">
             <img className="h-10 w-10 rounded-full
             grayscale hover:grayscale-0 cursor-pointer
             filter " src="https://i.imgur.com/e2yvD6A.png" alt="" />
